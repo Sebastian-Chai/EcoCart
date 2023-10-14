e06eee98094e7ac8396ab54e874e48939f4d16a0
Sebastian Chai Pin Zheng <105787056+Sebastian-Chai@users.noreply.github.com>
2023-10-14 22:29:50 +0800

    Adding Badges to Shopee Cards that are preferred by EcoCart

--- extensions/src/foreground.ts
+++ extensions/src/foreground.ts
@@ -1,211 +1,195 @@
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-nocheck
-// console.info('chrome-ext template-vanilla-js content script');
+console.info('chrome-ext template-vanilla-js content script');
 
-// // Switch for the popup
-// let activePopup = false;
-// let counter = 0;
-// let theNewssite, haveNetflix, theSport, theHobby;
+import { setStorageData, getStorageData } from './storage';
 
-// chrome.storage.local.get('newsSite', (data) => {
-//   if (chrome.runtime.lastError) {
-//     return;
-//   }
-//   theNewssite = data.newsSite;
-// });
-// chrome.storage.local.get('netFlix', (data) => {
-//   if (chrome.runtime.lastError) {
-//     return;
-//   }
-//   haveNetflix = data.netFlix;
-// });
-// chrome.storage.local.get('mySport', (data) => {
-//   if (chrome.runtime.lastError) {
-//     return;
-//   }
-//   theSport = data.mySport;
-// });
-// chrome.storage.local.get('myHobby', (data) => {
-//   if (chrome.runtime.lastError) {
-//     return;
-//   }
-//   theHobby = data.myHobby;
-// });
+// Switch for the popup
+let activePopup = false;
+let counter = 0;
+let theNewssite, haveNetflix, theSport, theHobby;
+const PREFERRED_BRANDS = [
+  'Uniqlo',
+  'MUJI',
+  'ITO EN',
+  'The Body Shop',
+  'Shiseido',
+  'Innisfree',
+  'Cosrx',
+  'H&M Conscious',
+  'Etude House',
+  'Amorepacific',
+  'Tata Power Solar',
+  'Himalaya Herbals',
+  'Neemrana Hotels',
+  'Dabur',
+  'Kao Corporation',
+  'Lotte Group',
+  'BYD',
+  'Haier',
+  'Samsung',
+  'Tencent',
+];
 
-// const makePopup = (function () {
-//   return function (score) {
-//     // Check if element exists, otherwise make one
-//     let poop = document.getElementById('alertPopup');
-//     if (poop) document.getElementById('alertPopup').remove();
+chrome.storage.local.get('newsSite', (data) => {
+  if (chrome.runtime.lastError) {
+    return;
+  }
+  theNewssite = data.newsSite;
+});
+chrome.storage.local.get('netFlix', (data) => {
+  if (chrome.runtime.lastError) {
+    return;
+  }
+  haveNetflix = data.netFlix;
+});
+chrome.storage.local.get('mySport', (data) => {
+  if (chrome.runtime.lastError) {
+    return;
+  }
+  theSport = data.mySport;
+});
+chrome.storage.local.get('myHobby', (data) => {
+  if (chrome.runtime.lastError) {
+    return;
+  }
+  theHobby = data.myHobby;
+});
 
-//     poop = document.createElement('div');
-//     poop.setAttribute('id', 'alertPopup');
+const makePopup = (function () {
+  return function (score) {
+    // Check if element exists, otherwise make one
+    let poop = document.getElementById('alertPopup');
+    if (poop) document.getElementById('alertPopup').remove();
 
-//     document.body.prepend(poop);
-//     document.documentElement.classList.add('alertedPopup');
+    poop = document.createElement('div');
+    poop.setAttribute('id', 'alertPopup');
 
-//     // This would be so much easier with jQuery but lets not load a whole library to do one thing
-//     // $(poop).load("theHtmlFile.html"); though
-//     let request = new XMLHttpRequest();
+    document.body.prepend(poop);
+    document.documentElement.classList.add('alertedPopup');
 
-//     // eslint-disable-next-line no-undef
-//     request.open('GET', chrome.runtime.getURL('overlay.html'), true);
-//     request.onload = function () {
-//       if (request.status >= 200 && request.status < 400) {
-//         let resp = request.responseText;
-//         poop.innerHTML = resp;
-//         genPopup(poop, score);
-//       }
-//     };
+    // This would be so much easier with jQuery but lets not load a whole library to do one thing
+    // $(poop).load("theHtmlFile.html"); though
+    const request = new XMLHttpRequest();
 
-//     request.send();
+    // eslint-disable-next-line no-undef
+    request.open('GET', chrome.runtime.getURL('overlay.html'), true);
+    request.onload = function () {
+      if (request.status >= 200 && request.status < 400) {
+        let resp = request.responseText;
+        poop.innerHTML = resp;
+        genPopup(poop, score);
+      }
+    };
 
-//     const genPopup = function (elem, score) {
-//       const populateNav = function (theNav) {
-//         if (theNav) {
-//           if (haveNetflix)
-//             theNav.innerHTML += '<a href="http://netflix.com">Netflix</a>';
-//           theNav.innerHTML += newsLink(theNewssite);
-//           theNav.innerHTML +=
-//             '<span>' +
-//             theSport[0].toUpperCase() +
-//             theSport.substring(1) +
-//             '</span>';
-//           theNav.innerHTML +=
-//             '<span>' +
-//             theHobby[0].toUpperCase() +
-//             theHobby.substring(1) +
-//             '</span>';
-//         }
+    request.send();
 
-//         function newsLink(theNewssite) {
-//           let link;
-//           switch (theNewssite) {
-//             case 'gnn':
-//               link =
-//                 '<a href="http://www.goodnewsnetwork.org">Good News Network</a>';
-//               break;
-//             case 'pn':
-//               link = '<a href="http://www.positive.news">Positive News</a>';
-//               break;
-//             case 'od':
-//               link =
-//                 '<a href="http://www.optimistdaily.com">Optimist Daily</a>';
-//               break;
-//             case 'reddit':
-//               link =
-//                 '<a href="http://reddit.com/r/UpliftingNews">r/UpliftingNews</a>';
-//               break;
-//             default:
-//               link = '';
-//               break;
-//           } //switch
-//           return link;
-//         }
-//       };
-//       // score should range from -0.5 to 0.5 (normalized)
-//       // lower is worse, if 100% turn off
-//       // go back to [0, 1] then subtract
-//       let displayValue = 100 * (1 - (score + 0.5)); // % to display
-//       elem.querySelectorAll('.popupContent .pSc')[0].innerHTML =
-//         displayValue.toFixed(0) + '%';
-//       populateNav(elem.querySelectorAll('.popupContent nav')[0]);
+    const genPopup = function (elem, score) {
+      const populateNav = function (theNav) {
+        if (theNav) {
+          if (haveNetflix)
+            theNav.innerHTML += '<a href="http://netflix.com">Netflix</a>';
+          theNav.innerHTML += newsLink(theNewssite);
+          theNav.innerHTML +=
+            '<span>' +
+            theSport[0].toUpperCase() +
+            theSport.substring(1) +
+            '</span>';
+          theNav.innerHTML +=
+            '<span>' +
+            theHobby[0].toUpperCase() +
+            theHobby.substring(1) +
+            '</span>';
+        }
 
-//       let close1 = elem.querySelectorAll('.popupContent .cls')[0];
-//       let close2 = elem.querySelectorAll('.popupContent .fx')[0];
+        function newsLink(theNewssite) {
+          let link;
+          switch (theNewssite) {
+            case 'gnn':
+              link =
+                '<a href="http://www.goodnewsnetwork.org">Good News Network</a>';
+              break;
+            case 'pn':
+              link = '<a href="http://www.positive.news">Positive News</a>';
+              break;
+            case 'od':
+              link =
+                '<a href="http://www.optimistdaily.com">Optimist Daily</a>';
+              break;
+            case 'reddit':
+              link =
+                '<a href="http://reddit.com/r/UpliftingNews">r/UpliftingNews</a>';
+              break;
+            default:
+              link = '';
+              break;
+          } //switch
+          return link;
+        }
+      };
+      // score should range from -0.5 to 0.5 (normalized)
+      // lower is worse, if 100% turn off
+      // go back to [0, 1] then subtract
+      // populateNav(elem.querySelectorAll('.popupContent nav')[0]);
 
-//       close1.style.visibility = 'hidden';
-//       setTimeout(function () {
-//         close1.style.visibility = 'visible';
-//         close2.style.visibility = 'hidden';
-//       }, 10000); //10s timer
+      let close1 = elem.querySelectorAll('.popupContent .cls')[0];
+      console.log('>>> gay >>>');
+      console.log(close1);
+      let close2 = elem.querySelectorAll('.popupContent .fx')[0];
+      console.log(close2);
 
-//       // Bind events
-//       close1.addEventListener('click', removePopup);
-//       close2.addEventListener('click', forceClosePopup);
+      close1.style.visibility = 'hidden';
+      setTimeout(function () {
+        close1.style.visibility = 'visible';
+        close2.style.visibility = 'hidden';
+      }, 3000); //10s timer
 
-//       // Change color if darkmode
-//       let bgColor = window
-//         .getComputedStyle(document.body)
-//         .getPropertyValue('background-color');
-//       document.documentElement.style.setProperty('--background', bgColor);
-//       document.documentElement.style.setProperty(
-//         '--transparency',
-//         'rgba(' + bgColor.split('(')[1].split(')')[0] + ',0.5)',
-//       );
-//       if (!(bgColor == '#ffffff' || bgColor == 'rgb(255, 255, 255)')) {
-//         document.documentElement.style.setProperty('--text', '#ffffff');
-//         document.documentElement.style.setProperty(
-//           '--petalb',
-//           'rgba(255,255,255,0.3)',
-//         );
-//       }
+      // Bind events
+      close1.addEventListener('click', removePopup);
+      close2.addEventListener('click', forceClosePopup);
 
-//       if (document.getElementsByClassName('petalbloom')[0] === undefined)
-//         return;
-//       // Kill some plants yo
-//       killFlower(score);
-//     };
-//   };
-// })();
+      if (document.getElementsByClassName('petalbloom')[0] === undefined)
+        return;
+      // Kill some plants yo
+      killFlower(score);
+    };
+  };
+})();
 
-// // Function to kill the plants
-// let killFlower = function (score) {
-//   if (score <= 1)
-//     document.getElementsByClassName('pb1')[0].classList.add('dead');
-//   else document.getElementsByClassName('pb1')[0].classList.remove('dead');
+// Function to regen petals over time
+let tHnd; // global so that I can turn it off elsewhere
 
-//   if (score <= 2)
-//     document.getElementsByClassName('pb2')[0].classList.add('dead');
-//   else document.getElementsByClassName('pb2')[0].classList.remove('dead');
+// Function to remove the popup gently and reset the score
+let removePopup = function () {
+  const thePopup = document.getElementById('alertPopup');
+  if (thePopup) thePopup.remove();
 
-//   if (score <= 3)
-//     document.getElementsByClassName('pb3')[0].classList.add('dead');
-//   else document.getElementsByClassName('pb3')[0].classList.remove('dead');
+  document.documentElement.classList.remove('alertedPopup');
 
-//   if (score <= 4)
-//     document.getElementsByClassName('pb4')[0].classList.add('dead');
-//   else document.getElementsByClassName('pb4')[0].classList.remove('dead');
+  // Reset score
+  totalScore = 0;
+  activePopup = false;
+  counter = 0;
+};
 
-//   if (score <= 5)
-//     document.getElementsByClassName('pb5')[0].classList.add('dead');
-//   else document.getElementsByClassName('pb5')[0].classList.remove('dead');
-// };
+// Function to force the popup to close
+const forceClosePopup = function () {
+  let thePopup = document.getElementById('alertPopup');
+  if (thePopup) thePopup.remove();
 
-// // Function to regen petals over time
-// let tHnd; // global so that I can turn it off elsewhere
+  document.documentElement.classList.remove('alertedPopup');
 
-// // Function to remove the popup gently and reset the score
-// let removePopup = function () {
-//   let thePopup = document.getElementById('alertPopup');
-//   if (thePopup) thePopup.remove();
+  // Turn off the popup trigger
+  activePopup = false;
 
-//   document.documentElement.classList.remove('alertedPopup');
+  // Set timer to turn it on again
+  activePopup = setTimeout(function () {
+    return true;
+  }, 10000);
 
-//   // Reset score
-//   totalScore = 0;
-//   activePopup = false;
-//   counter = 0;
-// };
-
-// // Function to force the popup to close
-// let forceClosePopup = function () {
-//   let thePopup = document.getElementById('alertPopup');
-//   if (thePopup) thePopup.remove();
-
-//   document.documentElement.classList.remove('alertedPopup');
-
-//   // Turn off the popup trigger
-//   activePopup = false;
-
-//   // Set timer to turn it on again
-//   activePopup = setTimeout(function () {
-//     return true;
-//   }, 10000);
-
-//   // Stop petals from regenerating
-//   clearInterval(tHnd);
-// };
+  // Stop petals from regenerating
+  clearInterval(tHnd);
+};
 
 // Get the wrapper element
 function getWrapper() {
@@ -221,10 +205,29 @@ function doesProductExistInDatabase() {
   return false;
 }
 
-// Function to call on product page
+function convertCO2ToUnit(grams) {
+  const units = [
+    { label: 'cigarettes', conversion: 21.8 },
+    { label: 'car miles', conversion: 404 },
+    { label: 'smartphone charges', conversion: 0.5 },
+    // Add more units and their conversion factors as needed
+  ];
+
+  const convertedUnits = units.map((unit) => {
+    // round to 2 decimal places
+    const quantity = Math.round((grams / unit.conversion) * 100) / 100;
+
+    return `${quantity} ${unit.label}`;
+  });
+
+  return convertedUnits;
+}
+
+// Function to call on product pagex
 const runScriptProductPage = (function () {
   return async function (wrapper) {
     try {
+      console.log(await getStorageData());
       // letiables
       const productInformation = {
         categories: [],
@@ -265,10 +268,22 @@ const runScriptProductPage = (function () {
       productTitleWrapper.parentElement.prepend(
         await addReinforcement(doesProductExistInDatabase, productInformation),
       );
-      makePopup(0.2);
+
+      for (const brand of PREFERRED_BRANDS) {
+        if (productInformation['Product Name']?.includes(brand)) {
+          makePopup(await getStorageData().score);
+        }
+      }
     } catch (err) {
       console.log(err);
     }
+  };
+})();
+
+const runScriptSearchPage = (function () {
+  return async function (wrapper) {
+    console.log('DOOMED');
+    console.log(wrapper);
   };
 })();
 
@@ -277,19 +292,12 @@ const addReinforcement = async function (
   dataExistInDatabase = false,
   productInformation = {},
 ) {
+  const arbitaryNumber = Math.floor(Math.random() * (1000 - 100) + 300);
+  const relativeUnits = convertCO2ToUnit(arbitaryNumber);
   const node = document.createElement('div');
   node.innerHTML = `
-    <div id="ecocart-flag" class="ecocart-banner" style="border: 1px solid let(--petalc); color: let(--petalc); background: #CBF0C1; /* padding: 1rem; */ font: 0.9rem sans-serif; width: 100%; margin-bottom: 1rem; padding-left: 1rem; padding-top: 1rem; padding-bottom: 1rem; border-radius: 5px;">
-      <div style="display: flex; justify-content: center; align-items: center;">
-        <img src="https://ecocart-lifehack2023.netlify.app/favicon.ico" style="height: 17px; width: 17px; margin-right: 10px;" />
-        <h1 style="font-size: 12px;">EcoCart</h1>
-      </div>  
+    <div id="ecocart-flag" class="ecocart-banner" style="border: 1px solid let(--petalc); color: let(--petalc); background: #CBF0C1; padding: 1rem; font: 0.9rem sans-serif; margin-bottom: 1rem; padding-left: 1rem; padding-top: 1rem; padding-bottom: 1rem; border-radius: 5px;">
       <span style="display: flex; justify-content: center; align-items: center;">
-        <p style="text-align: center;">
-          Did you the process of making '${
-            productInformation['Product Name']
-          }' produces <b>1000kg</b> of CO2? That is equivalent to driving a car (10km) or charging your phone (1000 times)!
-        </p>
         ${
           dataExistInDatabase &&
           `<span class="tooltip">
@@ -297,7 +305,18 @@ const addReinforcement = async function (
             <span class="tooltiptext">Source from <a style="color: #79afe0; text-decoration: underline; " href="https://www.co2everything.com" target="_blank" rel="noopener noreferrer">CO2</a></span>
           </span>`
         }
+        <p>
+          Did you the process of making '${
+            productInformation['Product Name']
+          }' produces <b>${arbitaryNumber}</b> of CO2? That is equivalent to <b>${relativeUnits.join(
+    ', ',
+  )}</b>!
+        </p>
       </span>
+      <div style="display: flex; justify-content: flex-end; align-items: center;">
+        <img src="https://ecocart-lifehack2023.netlify.app/favicon.ico" style="height: 17px; width: 17px; margin-right: 10px;" />
+        <h1 style="font-size: 12px;">EcoCart</h1>
+      </div>  
     </div>`;
 
   return node;
@@ -335,12 +354,18 @@ const loadCss = function () {
       wrapper !== null &&
       wrapper.querySelector('.page-product__detail') !== null,
     function () {
+      console.log('I AM BEING INVOKED HERE');
       // Check if the url is a product page or search page by checking the url
       const url = window.location.href;
       const isSearchPage = url.includes('search');
 
+      console.log(isSearchPage);
       loadCss();
-      if (!isSearchPage) runScriptProductPage(getWrapper());
+      if (!isSearchPage) {
+        runScriptProductPage(getWrapper());
+      } else {
+        runScriptSearchPage(getWrapper());
+      }
     },
   );
 })();
