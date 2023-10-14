5f5eee185e94e8f79dff88fab1df69048cdbd4d0
Sebastian Chai Pin Zheng <105787056+Sebastian-Chai@users.noreply.github.com>
2023-10-14 22:16:45 +0800

    Configuring AWS SAM Configs

---
+++ backend/functions/calculate-total-co/app.py
@@ -0,0 +1,83 @@
+# packages
+import json
+import openai
+from decouple import config
+import re
+
+# modules
+from src.logger import logger
+
+openai.api_key = config("OPENAI_API_KEY")
+
+PROMPT_TEMPLATE = """
+Provide the total estimated carbon footprint of the given products:
+{product_titles}
+No matter what, you must give a rough estimate of the value of CO2 produced, infer from product titles and product type e.g. phones and electronics.
+
+Reply in the following template:
+Carbon Footprint: XXX kg CO2
+"""
+
+def lambda_handler(event, context):
+    """
+    lambda handler
+    """
+    logger.debug("Lambda function ARN:", context.invoked_function_arn)
+    logger.debug("CloudWatch log stream name:", context.log_stream_name)
+    logger.debug("CloudWatch log group name:",  context.log_group_name)
+    logger.debug("Lambda Request ID:", context.aws_request_id)
+    logger.debug("Lambda function memory limits in MB:", context.memory_limit_in_mb)
+
+    logger.debug("Event: %s", event)
+    body = event.get("body")
+
+    # Convert the body to a JSON object
+    if not isinstance(body, dict):
+        body = json.loads(body)
+
+    product_titles = body.get("product_titles") # list
+    assert type(product_titles) == list, "product_titles must be a list"
+    assert len(product_titles) > 0, "product_titles must not be empty"
+
+    # ----- QUERY GPT SECTION -----
+    prompt = PROMPT_TEMPLATE.format(product_titles=product_titles)
+    try:
+        completion = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[{"role": "user", "content": prompt}],
+            temperature=0.5,
+            presence_penalty=0.1,
+            frequency_penalty=0.1,
+        )
+        completion_content = completion.choices[0].message.get("content")
+        logger.info(f"GPT 3.5-Turbo Reply:\n{completion_content}")
+
+        # ----- POST-PROCESSING SECTION -----
+        # Search for the first match of the regex
+        match = re.search(
+            r"(\d{1,3}) kg CO2", completion_content
+        )
+        if match:
+            co2_footprint = int(match.group(1))
+            logger.info(f"CO2 Footprint: {co2_footprint}")
+        else:
+            co2_footprint = 0
+            logger.error("CO2 Footprint not found")
+
+        # Response
+        response = {
+            "statusCode": 200,
+            "headers": {
+                "Content-Type": "application/json",
+            },
+            "body": json.dumps(
+                {
+                    "completion_content": completion_content,
+                    "co2_footprint": co2_footprint,
+                }
+            )
+        }
+        return response
+    except Exception as exc:
+        logger.error(f"Grading Error: {exc}")
+        raise
